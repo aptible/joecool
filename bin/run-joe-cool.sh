2d5bd88ac94e8aa2dbe718fb8c2ad5cba241385c
Ashley Mathew <ashley@aptible.com>
2019-11-19 23:43:10 -0500

    Switch to filebeats.

--- bin/run-joe-cool.sh
+++ bin/run-joe-cool.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -o errexit
 
+: ${CONTAINERS:?"Error: environment variable CONTAINERS should contain a JSON-formatted list of container ids."}
 : ${LOGSTASH_ENDPOINT:?"Error: environment variable LOGSTASH_ENDPOINT should contain the Gentleman Jerry endpoint."}
 : ${LOGSTASH_CERTIFICATE:?"Error: environment variable LOGSTASH_CERTIFICATE should contain Gentleman Jerry's certificate."}
+
 
 if [ ! -d /tmp/dockerlogs ]; then
   echo "/tmp/dockerlogs should exist and contain Docker logs."
@@ -12,34 +14,22 @@ fi
 # We persist whether we sent all the logs via READ_FROM_BEGINNING into a file
 # (HAS_READ_FROM_BEGINNING). This ensures that if Joe Cool is restarted, we
 # don't re-send all the logs.
-
 HAS_READ_FROM_BEGINNING=/tmp/read-from-beginning
-
-TAIL_OPT="-tail=true"
-
+export TAIL=1
 if [[ -n "$READ_FROM_BEGINNING" ]]; then
   echo "READ_FROM_BEGINNING is TRUE"
 fi
-
 if [[ ! -f "$HAS_READ_FROM_BEGINNING" ]]; then
   echo "FILE NOT PRESENT is TRUE"
 fi
-
 if [[ -n "$READ_FROM_BEGINNING" ]] && [[ ! -f "$HAS_READ_FROM_BEGINNING" ]]; then
   touch "$HAS_READ_FROM_BEGINNING"
-  TAIL_OPT=""
+  export TAIL=0
 fi
 
-CONFIG=logstash-forwarder/logstash-forwarder.config
+CONFIG=${FILEBEAT_HOME}/filebeat.yml
+erb ${FILEBEAT_HOME}/filebeat.yml.erb > ${CONFIG}
+echo "$LOGSTASH_CERTIFICATE" > ${FILEBEAT_HOME}/logstash.crt
 
-if [[ -n "${JSON_CONFIGURATION:-}" ]]; then
-  ruby generate-config.rb > "$CONFIG"
-else
-  : ${CONTAINERS_TO_MONITOR:?"Error: environment variable CONTAINERS_TO_MONITOR should contain a comma-separated list of container ids."}
-  erb logstash-forwarder.config.erb > "$CONFIG"
-fi
 
-cd logstash-forwarder
-echo "$LOGSTASH_CERTIFICATE" > logstash.crt
-
-exec ./logstash-forwarder -config logstash-forwarder.config -max-line-bytes=101376 $TAIL_OPT
+exec ${FILEBEAT_HOME}/filebeat -c ${CONFIG} -e
